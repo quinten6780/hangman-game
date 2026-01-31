f8ae4fe9166aaad7911e85b98519713ba281bacc
quinten van den heuvel <q.b.vdheuvel@gmail.com>
2026-01-31 21:05:34 +0100

    1

---
+++ src/hangman.java
@@ -0,0 +1,39 @@
+
+
+class hangman {
+    
+    void game_main() {
+                // all constructors and main walktrough of game
+    }
+
+    void game_config(){
+            // welcome the player and gets game config     later: custom mode
+
+
+    }
+
+    void gen_word() {
+            // generate a random word for the usser to guess            later: gui
+
+
+
+    }
+
+    void guess_word() {
+            // makes the usser guess the word   
+
+
+
+    }
+
+    void report_highscore() {
+            // report back the stats and score    later: highscore system
+
+
+
+
+    }
+
+
+
+}
