7859aaf9f333ba9de33483a78d3e06992bcbf8c1
quinten van den heuvel <q.b.vdheuvel@gmail.com>
2026-01-31 22:43:55 +0100

    3

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 
 
 public class App {
-    public static boolean config = true;    // auto fill in variables, show config messages
+    public static boolean config_messages = true;    // show config messages
+    public static boolean config_user_name = true;   // auto fill user_name with config_name
+    public static boolean config_game_difficulty = true;   // auto fill game_difficulty with a
+
+ 
+
 
     public static void main(String[] args) throws Exception {
    
--- src/hangman.java
+++ src/hangman.java
@@ -10,26 +10,32 @@ class hangman {
     final Scanner scanner = new Scanner(System.in);
 
   
-    
     String user_name;
+    char game_difficulty;
+
     
-    void game_main() {                  // all constructors and main walktrough of game
+    void game_main() {      // all constructors and main walktrough of game
         game_config();
         
         System.out.println(user_name);
                                         
     }
 
-    void game_config(){             // welcome the player and gets game config     later: custom mode
+    void game_config(){    // welcome the player and gets game config     Later: custom mode
         System.out.println("\nWelcome to Hangman\n");
 
-        if (App.config == true) { user_name = "\nconfig_name"; }
+if (App.config_user_name == true) { user_name = "\nconfig_name"; }
         else {System.out.print("Your name: "); user_name = scanner.next();}
+if (App.config_messages == true) { System.out.println("CONFIG user_name =   "+user_name); }
 
-        if (App.config == true) { System.out.println("CONFIG user_name =   "+user_name); }
+
 
         System.out.println("Please choose your difficulty:\nA Easy mode\nB Medium mode\nC Hard mode");
+        System.out.print("Choose your difficulty: "); game_difficulty = scanner.next().charAt(0);
 
+
+        
+        
     
         
 
